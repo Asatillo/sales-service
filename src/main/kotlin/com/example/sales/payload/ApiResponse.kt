d31321fe898ec01d7824e00dacc96a37ced1937b
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-12-12 12:56:25 +0100

    fix the constructors issue

--- src/main/kotlin/com/example/sales/payload/ApiResponse.kt
+++ src/main/kotlin/com/example/sales/payload/ApiResponse.kt
@@ -2,29 +2,28 @@ package com.example.sales.payload
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
-import lombok.Getter
 import org.springframework.http.HttpStatus
 
-@Getter
 @JsonPropertyOrder(
     "success",
     "message"
 )
-class ApiResponse(
+class ApiResponse {
     @JsonPropertyOrder("success")
-    private var success: Boolean,
-    @JsonPropertyOrder("message")
-    private var message: String,
-    @JsonIgnore
-    private var status: HttpStatus
-){
+    private var success: Boolean
 
-    fun constructor(success: Boolean, message: String) {
+    @JsonPropertyOrder("message")
+    private var message: String
+
+    @JsonIgnore
+    private lateinit var status: HttpStatus
+
+    constructor(success: Boolean, message: String){
         this.success = success
         this.message = message
     }
 
-    fun constructor(success: Boolean, message: String, status: HttpStatus) {
+    constructor(success: Boolean, message: String, status: HttpStatus){
         this.success = success
         this.message = message
         this.status = status
