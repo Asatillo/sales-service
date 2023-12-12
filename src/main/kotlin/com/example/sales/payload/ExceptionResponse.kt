dcf7bc2485a333517db79dc10c064d6af8494df1
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-12-12 12:47:42 +0100

    fix the constructors issue

--- src/main/kotlin/com/example/sales/payload/ExceptionResponse.kt
+++ src/main/kotlin/com/example/sales/payload/ExceptionResponse.kt
@@ -4,27 +4,25 @@ import lombok.Getter
 import java.time.Instant
 
 @Getter
-class ExceptionResponse(
-    private var error: String,
-    private var status: Int,
-    private var message: String,
-    private var validationErrors: HashMap<String, String>,
-    private var timestamp: Instant,
-) {
+class ExceptionResponse {
+    private var error: String
+    private var status: Int
+    private var message: String
+    private var validationErrors: HashMap<String, String> = HashMap()
+    private var timestamp: Instant
 
-    fun constructor(message: String, error: String, status: Int) {
+    constructor(message: String, error: String, status: Int) {
         this.message = message
         this.error = error
         this.status = status
-        this.timestamp = Instant.now()
+        timestamp = Instant.now()
     }
 
-    fun constructor(message: String, error: String, status: Int, validationErrors: HashMap<String, String>) {
+    constructor(message: String, error: String, status: Int,  validationErrors: HashMap<String, String>) {
         this.message = message
         this.error = error
         this.status = status
         this.validationErrors = validationErrors
-        this.timestamp = Instant.now()
+        timestamp = Instant.now()
     }
-
 }
