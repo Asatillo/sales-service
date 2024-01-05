1ca51babbf77e9c4bbd3f5425294c3a4e6db2799
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-01-05 11:22:53 +0100

    add app constants

--- src/main/kotlin/com/example/sales/utils/AppConstants.kt
+++ src/main/kotlin/com/example/sales/utils/AppConstants.kt
@@ -6,7 +6,8 @@ class AppConstants {
         const val DEFAULT_PAGE_SIZE = 30
         const val MAX_PAGE_SIZE = 50
         const val DEFAULT_SORT_BY = "id"
-        const val PROMOTIONS_TYPES = "^(Percentage|Fixed)$"
+        const val SALE_ORDER_TYPE = "^(PLAN|PACKAGE|DEVICE|ACCESSORIES|ALL)$"
+        const val PROMOTIONS_TYPES = "^(PERCENTAGE|FIXED)$"
         const val DEVICE_TYPES = "^(MOBILE|ROUTER)$"
         const val DECISION_TYPES = "^(ACCEPTED|REJECTED|PENDING)$"
         const val COMMUNICATION_TYPES = "^(SMS|EMAIL|CALL|IN-PERSON)$"
