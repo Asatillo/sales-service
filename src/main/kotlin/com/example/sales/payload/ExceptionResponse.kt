02cd4af71ace8af6db1a4176f4a67cc98be99115
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-02-13 16:59:49 +0100

    minimize ExceptionResponse

--- src/main/kotlin/com/example/sales/payload/ExceptionResponse.kt
+++ src/main/kotlin/com/example/sales/payload/ExceptionResponse.kt
@@ -2,26 +2,6 @@ package com.example.sales.payload
 
 import java.time.Instant
 
-@Getter
-class ExceptionResponse {
-    private var error: String
-    private var status: Int
-    private var message: String
-    private var validationErrors: HashMap<String, String> = HashMap()
-    private var timestamp: Instant
-
-    constructor(message: String, error: String, status: Int) {
-        this.message = message
-        this.error = error
-        this.status = status
-        timestamp = Instant.now()
-    }
-
-    constructor(message: String, error: String, status: Int,  validationErrors: HashMap<String, String>) {
-        this.message = message
-        this.error = error
-        this.status = status
-        this.validationErrors = validationErrors
-        timestamp = Instant.now()
-    }
+class ExceptionResponse(val message: String, val error: String, val status: Int) {
+    val timestamp: Instant = Instant.now()
 }
