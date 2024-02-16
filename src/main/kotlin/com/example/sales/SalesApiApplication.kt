725e2093f42db7da520e3ca7bcb101f2b0c57dd7
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-02-16 15:59:39 +0100

    import feign

--- src/main/kotlin/com/example/sales/SalesApiApplication.kt
+++ src/main/kotlin/com/example/sales/SalesApiApplication.kt
@@ -3,9 +3,11 @@ package com.example.sales
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 class SalesApplication
 
 fun main(args: Array<String>) {
