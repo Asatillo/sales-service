41f370a9a8d5370a9e83aabc1ccccb275472b47f
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-03-04 11:07:07 +0100

    add a query to find all sales with search

--- src/main/kotlin/com/example/sales/repository/SaleRepository.kt
+++ src/main/kotlin/com/example/sales/repository/SaleRepository.kt
@@ -4,7 +4,11 @@ import com.example.sales.model.Sale
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface SaleRepository: JpaRepository<Sale, Long> {
     fun findByCustomerId(id: Long, pageable: PageRequest): Page<Sale>
+
+    @Query("SELECT s FROM Sale s WHERE CONCAT(s.description, ' ', s.customer, ' ', s.productType, ' ', s.paymentProgress) LIKE %:search%")
+    fun findAllWithSearch(search: String, pageable: PageRequest): Page<Sale>
 }
